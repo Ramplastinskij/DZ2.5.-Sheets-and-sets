28c90a7601070c18abe4559f92b085527c57b5fb
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-11-17 18:51:34 +0500

    Initial commit

---
+++ src/test/java/pro/sky/DZ25/_Sheets/and/sets/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.DZ25._Sheets.and.sets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
